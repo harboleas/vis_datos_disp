1f614262cccd7411e92f7d513031affb4e272e5c
Hugo Arboleas <hugo.arboleas@gmail.com>
2022-05-20 17:03:24 -0300

    add simu

---
+++ simu_datos_disp/simu_datos_disp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gtk;
+
+namespace simu_datos_disp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+        }
+    }
+}
